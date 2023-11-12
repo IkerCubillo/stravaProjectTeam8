f10b13221feccb2527b973c49f79a9285941cf69
Kerman Bruña <kerman.bruna@opendeusto.es>
2023-11-12 21:52:06 +0100

    IRemoteFacade

--- 4_eAuctions_Server_v1/src/es/deusto/ingenieria/sd/auctions/server/remote/IRemoteFacade.java
+++ 4_eAuctions_Server_v1/src/es/deusto/ingenieria/sd/auctions/server/remote/IRemoteFacade.java
@@ -12,22 +12,6 @@ import es.deusto.ingenieria.sd.auctions.server.data.dto.CategoryDTO;
 import es.deusto.ingenieria.sd.auctions.server.data.dto.ChallengeDTO;
 import es.deusto.ingenieria.sd.auctions.server.data.dto.TrainingSessionDTO;
 
-//This interface defines the API of the Server. It represents the Remote Facade pattern
-//public interface IRemoteFacade extends Remote {	
-//
-//	public long login(String email, String password) throws RemoteException;
-//	
-//	public void logout(long token) throws RemoteException; 
-//	
-//	public List<CategoryDTO> getCategories() throws RemoteException;
-//	
-//	public List<ArticleDTO> getArticles(String aCategory) throws RemoteException;
-//	
-//	public boolean makeBid(long token, int article, float amount) throws RemoteException;
-//	
-//	public float getUSDRate() throws RemoteException;
-//	
-//	public float getGBPRate() throws RemoteException;	
 
 	public interface IRemoteFacade extends Remote {	
 	

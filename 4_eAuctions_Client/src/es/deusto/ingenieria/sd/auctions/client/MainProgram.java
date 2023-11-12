701bdd80a531ca93ea11cf1bfdb19bd437caae3e
Kerman Bruña <kerman.bruna@opendeusto.es>
2023-11-12 22:35:28 +0100

    Main program

--- 4_eAuctions_Client/src/es/deusto/ingenieria/sd/auctions/client/MainProgram.java
+++ 4_eAuctions_Client/src/es/deusto/ingenieria/sd/auctions/client/MainProgram.java
@@ -1,19 +1,31 @@
 package es.deusto.ingenieria.sd.auctions.client;
 
+import java.rmi.RemoteException;
+import java.sql.Date;
+import java.sql.Time;
 import java.util.List;
 
-import es.deusto.ingenieria.sd.auctions.client.controller.BidController;
+import es.deusto.ingenieria.sd.auctions.client.controller.MainController;
 import es.deusto.ingenieria.sd.auctions.client.controller.LoginController;
-import es.deusto.ingenieria.sd.auctions.client.gui.BidWindow;
+import es.deusto.ingenieria.sd.auctions.client.gui.MainWindow;
 import es.deusto.ingenieria.sd.auctions.client.gui.LoginWindow;
 import es.deusto.ingenieria.sd.auctions.client.remote.ServiceLocator;
-import es.deusto.ingenieria.sd.auctions.server.data.dto.ArticleDTO;
-import es.deusto.ingenieria.sd.auctions.server.data.dto.CategoryDTO;
+import es.deusto.ingenieria.sd.auctions.server.data.domain.Challenge;
+import es.deusto.ingenieria.sd.auctions.server.data.dto.ChallengeDTO;
+import es.deusto.ingenieria.sd.auctions.server.data.dto.TrainingSessionDTO;
+import es.deusto.ingenieria.sd.auctions.server.remote.RemoteFacade;
 
 public class MainProgram {
 
-	public static void main(String[] args) {	
+	public static void main(String[] args) throws RemoteException {	
 		ServiceLocator serviceLocator = new ServiceLocator();
+		RemoteFacade remoteFacade = new RemoteFacade();
+		String name = null;
+		Date start = null;
+		Date end = null;
+		float metric = 0;
+		String sportType = null;
+		Challenge c = null;
 		
 		//args[0] = RMIRegistry IP
 		//args[1] = RMIRegistry Port
@@ -22,23 +34,28 @@ public class MainProgram {
 		
 		LoginController loginController = new LoginController(serviceLocator);
 		LoginWindow loginDialog = new LoginWindow(loginController);			
-		BidController bidController = new BidController(serviceLocator);			
-		BidWindow bidWindow = new BidWindow(bidController);
+		MainController mainController = new MainController(remoteFacade);			
+		MainWindow mainWindow = new MainWindow(mainController);
 		
 		//Login
 		loginDialog.login();		
 		//Get Categories
-		List<CategoryDTO> categories = bidWindow.getCategories();
+		List<ChallengeDTO> challenges = mainWindow.getChallenges();
 		//Get Articles of a category (first category is selected)
-		List<ArticleDTO> articles = bidWindow.getArticles(categories.get(0).getName());
+		List<TrainingSessionDTO> trainingSessions = mainWindow.getTrainingSession();
 		//Convert currency to GBP
-		bidWindow.currencyToGBP(articles);
+		mainWindow.setupDistanceChallenge(name, start, end, metric, sportType);
 		//Convert currency to USD
-		bidWindow.currencyToUSD(articles);
+		mainWindow.setupActivityTimeChallenge(name, start, end, metric, sportType);
 		//Place a bid (first article of the category is selected; the token is stored in the BidController)
-		bidWindow.makeBid(loginController.getToken(), articles.get(0));
-		//Get Articles to check if the bid has been done
-		articles = bidWindow.getArticles(categories.get(0).getName());
+		mainWindow.acceptChallenge(c);
+		String title = null;
+		String sport = null;
+		float distance = 0;
+		Date startDate = null;
+		Time timeStart = null;
+		float duration = 0;
+		mainWindow.createSession(title, sport, distance, startDate, timeStart, duration);
 		//Logout
 		loginDialog.logout();
 	}

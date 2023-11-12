2403452781b7fd8c8de61b9ee89b53007b6ab04c
fletch <fletchergunderson@gmail.com>
2023-11-12 19:32:34 +0100

    User sort of finished, still need to add items

--- 4_eAuctions_Server_v1/src/es/deusto/ingenieria/sd/auctions/server/data/dto/User1DTO.java
+++ 4_eAuctions_Server_v1/src/es/deusto/ingenieria/sd/auctions/server/data/dto/User1DTO.java
@@ -1,5 +1,22 @@
 package es.deusto.ingenieria.sd.auctions.server.data.dto;
 
-public class User1DTO {
+import java.sql.Date;
 
+public class User1DTO {
+	private String email;
+	private String account;
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getAccount() {
+		return account;
+	}
+	public void setAccount(String account) {
+		this.account = account;
+	}
 }

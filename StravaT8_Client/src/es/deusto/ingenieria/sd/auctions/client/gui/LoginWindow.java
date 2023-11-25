9328a1064b56473eb31b0e5684f019308aa5320c
Kerman Bruña <kerman.bruna@opendeusto.es>
2023-11-25 22:05:49 +0100

    register parameters

--- StravaT8_Client/src/es/deusto/ingenieria/sd/auctions/client/gui/LoginWindow.java
+++ StravaT8_Client/src/es/deusto/ingenieria/sd/auctions/client/gui/LoginWindow.java
@@ -39,10 +39,12 @@ public class LoginWindow {
 	}
 
 	public void register() {
-		System.out.println(" - Login into the server: '" + this.email + "' - '" + this.password + "' ...");
+		System.out.println(" - Registering into the server: '" + this.account + "' - '" + this.email + "' - '" + this.password 
+				+ "' - '" + this.name + "' - '" + this.birthdate + "' - '" + this.weight + "' - '" + this.height + "' - '" + this.mbpm 
+				+ "' - '" + this.bpm + "' ...");
 		String sha1 = org.apache.commons.codec.digest.DigestUtils.sha1Hex(password);
 		System.out.println("\t* Password hash: " + sha1);
-		boolean result = this.controller.register( account,  email,  name,  birthdate,  weight,  height,  mbpm,  bpm);
+		boolean result = this.controller.register(account, email, name, birthdate, weight, height, mbpm, bpm);
 		System.out.println("\t* Login result: " + result);
 		System.out.println("\t* Token: " + this.controller.getToken());
 		

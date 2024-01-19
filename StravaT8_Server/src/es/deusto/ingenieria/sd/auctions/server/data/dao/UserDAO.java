341428ad5fb36e4b161a6fe74529c279d63ff13e
kermanbruna <kerman.bruna@opendeusto.es>
2024-01-19 17:40:19 +0100

    User DAO

--- StravaT8_Server/src/es/deusto/ingenieria/sd/auctions/server/data/dao/UserDAO.java
+++ StravaT8_Server/src/es/deusto/ingenieria/sd/auctions/server/data/dao/UserDAO.java
@@ -1,5 +1,130 @@
 package es.deusto.ingenieria.sd.auctions.server.data.dao;
 
-public class UserDAO {
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Query;
+
+import es.deusto.ingenieria.sd.auctions.server.data.domain.User;
+
+public class UserDAO extends DataAccessObject implements IDataAccessObject<User>{
+	
+	private static UserDAO instance;	
+	
+	private UserDAO() { }
+	
+	public static UserDAO getInstance() {
+		if (instance == null) {
+			instance = new UserDAO();
+		}		
+		
+		return instance;
+	}	
+	
+	@Override
+	public void save(User object) {
+		User storedObject = instance.find(object.getEmail());
+
+		EntityManager em = emf.createEntityManager();
+		EntityTransaction tx = em.getTransaction();
+
+		try {
+			tx.begin();
+			
+			if (storedObject != null) {
+				em.merge(object);
+			} else {
+				em.persist(object);
+			}
+			
+			tx.commit();
+		} catch (Exception ex) {
+			System.out.println("  $ Error storing User: " + ex.getMessage());
+		} finally {
+			if (tx != null && tx.isActive()) {
+				tx.rollback();
+			}
+
+			em.close();
+		}
+	}
+
+	@Override
+	public void delete(User object) {
+		EntityManager em = emf.createEntityManager();
+		EntityTransaction tx = em.getTransaction();
+
+		try {
+			tx.begin();
+			
+			User storedObject = (User) em.find(User.class, object.getEmail());
+			em.remove(storedObject);
+			
+			tx.commit();
+		} catch (Exception ex) {
+			System.out.println("  $ Error removing User: " + ex.getMessage());
+		} finally {
+			if (tx != null && tx.isActive()) {
+				tx.rollback();
+			}
+
+			em.close();
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<User> getAll() {
+		EntityManager em = emf.createEntityManager();
+		EntityTransaction tx = em.getTransaction();
+		
+		List<User> users = new ArrayList<>();
+
+		try {
+			tx.begin();
+
+			Query q = em.createQuery("SELECT u FROM User u");
+			users = (List<User>) q.getResultList();
+						
+			tx.commit();
+		} catch (Exception ex) {
+			System.out.println("  $ Error querying users: " + ex.getMessage());
+		} finally {
+			if (tx != null && tx.isActive()) {
+				tx.rollback();
+			}
+
+			em.close();
+		}
+
+		return users;
+	}
+
+	@Override
+	public User find(String param) {		
+		EntityManager em = emf.createEntityManager();
+		EntityTransaction tx = em.getTransaction();
+
+		User result = null; 
+
+		try {
+			tx.begin();
+
+			result = (User) em.find(User.class, param);
+			
+			tx.commit();
+		} catch (Exception ex) {
+			System.out.println("  $ Error querying User by email: " + ex.getMessage());
+		} finally {
+			if (tx != null && tx.isActive()) {
+				tx.rollback();
+			}
+
+			em.close();
+		}
+
+		return result;
+	}
 }

de091d6930a05b0c9f184e92a25deda7fe1b7b56
kermanbruna <kerman.bruna@opendeusto.es>
2024-01-19 17:08:39 +0100

    DAO Interface

--- StravaT8_Server/src/es/deusto/ingenieria/sd/auctions/server/data/dao/IDataAccessObject.java
+++ StravaT8_Server/src/es/deusto/ingenieria/sd/auctions/server/data/dao/IDataAccessObject.java
@@ -1,5 +1,10 @@
 package es.deusto.ingenieria.sd.auctions.server.data.dao;
 
-public interface IDataAccessObject {
+import java.util.List;
 
+public interface IDataAccessObject<DomainObject> {
+	public void save(DomainObject object);
+	public void delete(DomainObject object);
+	public List<DomainObject> getAll();
+	public DomainObject find(String param);
 }

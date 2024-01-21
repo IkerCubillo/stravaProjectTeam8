bfca5023c5eac78b354139a6a61c8bd6c3c19ced
kermanbruna <kerman.bruna@opendeusto.es>
2024-01-21 13:35:00 +0100

    Gateway Factory singleton

--- StravaT8_Server/src/es/deusto/ingenieria/sd/auctions/server/gateway/GatewayFactory.java
+++ StravaT8_Server/src/es/deusto/ingenieria/sd/auctions/server/gateway/GatewayFactory.java
@@ -2,6 +2,8 @@ package es.deusto.ingenieria.sd.auctions.server.gateway;
 
 public class GatewayFactory {
 	
+	private static GatewayFactory instance;
+	
 	public static IGateway createGateway(String account) {
 		if (account.equals("Facebook")) {
 			return FacebookGateway.getInstance();
@@ -11,4 +13,12 @@ public class GatewayFactory {
 			return null;
 		}
 	}
+	
+	public static GatewayFactory getInstance() {
+		if (instance == null) {
+			instance = new GatewayFactory();
+		}
+		return instance;
+	}
+	
 }

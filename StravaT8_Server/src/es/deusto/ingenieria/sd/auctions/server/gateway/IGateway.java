6b3a3d21e1799d27ea3034133ad46aefd8acc946
IkerCubillo <113512116+IkerCubillo@users.noreply.github.com>
2024-01-22 02:03:26 +0100

    Delete StravaT8_Server directory
    deleting

--- StravaT8_Server/src/es/deusto/ingenieria/sd/auctions/server/gateway/IGateway.java
+++
@@ -1,6 +0,0 @@
-package es.deusto.ingenieria.sd.auctions.server.gateway;
-
-public interface IGateway {
-	public boolean userValidation(String email);
-	public boolean passwordValidation(String email, String password);
-}
--- StravaT8_Server/src/es/deusto/ingenieria/sd/auctions/server/mailsender/ISenderGateway.java
+++
@@ -1,6 +0,0 @@
-package es.deusto.ingenieria.sd.auctions.server.mailsender;
-
-public interface ISenderGateway {
-
-	public String sendMessage(String text);
-}
